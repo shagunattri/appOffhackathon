b5c21685c281ad933cfa14f6683ec2af8f955406
id10t <shagunattri17@gmail.com>
2019-11-30 18:42:03 +0530

    personal bot addition in dashboard

---
+++ finQ/project/dets/bot.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['detsuid']==0)) {
+  header('location:logout.php');
+  } else{
+
+if(isset($_POST['submit']))
+  {
+  	$userid=$_SESSION['detsuid'];
+    $dateincome=$_POST['dateincome'];
+    $type=$_POST['type'];
+     $amount=$_POST['amount'];
+    $query=mysqli_query($con, "insert into tbincome(UserId,IncomeDate,Type,Amount) value('$userid','$dateincome','$type','$amount')");
+if($query){
+echo "<script>alert('income has been added');</script>";
+echo "<script>window.location.href='manage-income.php'</script>";
+} else {
+echo "<script>alert('Something went wrong. Please try again');</script>";
+
+}
+  
+}
+  ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>finQ  || Add Income</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/datepicker3.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+	
+	<!--Custom Font-->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+	<link href="img/logo.png" rel="icon">
+  <link href="img/logo.png" rel="apple-touch-icon">
+</head>
+<body>
+	<?php include_once('includes/header.php');?>
+	<?php include_once('includes/sidebar.php');?>
+		
+	<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#">
+					<em class="fa fa-home"></em>
+				</a></li>
+				<li class="active">Personal Assistant</li>
+			</ol>
+		</div><!--/.row-->
+		
+		
+				
+		
+		<div class="row">
+			<div class="col-lg-12">
+			
+				
+				
+				<div class="panel panel-default">
+					<div class="panel-heading">Personal Assistant</div>
+					<div class="panel-body">
+						<p style="font-size:16px; color:red" align="center"> <?php if($msg){
+    echo $msg;
+  }  ?> </p>
+						<div class="col-md-12">
+                        <iframe width="1490" height="730" allow="microphone;" src="https://console.dialogflow.com/api-client/demo/embedded/f8b53a7f-42a2-4db6-b5db-f0b31de9eeb8"></iframe>
+						
+						</div>
+					</div>
+				</div><!-- /.panel-->
+			</div><!-- /.col-->
+			<?php include_once('includes/footer.php');?>
+		</div><!-- /.row -->
+	</div><!--/.main-->
+	
+<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/chart.min.js"></script>
+	<script src="js/chart-data.js"></script>
+	<script src="js/easypiechart.js"></script>
+	<script src="js/easypiechart-data.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="js/custom.js"></script>
+	
+</body>
+</html>
+<?php }  ?>
